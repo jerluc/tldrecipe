235d23c2b27c7a1feacc626f4f189f7a198777de
jerluc <me@jerluc.com>
2023-12-25 16:46:05 -0800

    Allow extra options to deploy CLI

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcloud functions deploy fetchRecipe --runtime=nodejs16 --trigger-http --region us-west1 --allow-unauthenticated
+gcloud functions deploy fetchRecipe --runtime=nodejs16 --trigger-http --region us-west1 --allow-unauthenticated $@
