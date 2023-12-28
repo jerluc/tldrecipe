76e75b11f0cd9e3f47d338f2c44a6e6f58f274cb
jerluc <me@jerluc.com>
2023-12-27 19:00:39 -0800

    [CI] Fix build and deploy scripts for PWA

--- pwa/build.sh
+++ pwa/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t gcr.io/tldrecipe/pwa . && docker push gcr.io/tldrecipe/pwa
+docker build --platform linux/amd64 -t gcr.io/tldrecipe/pwa . && docker push gcr.io/tldrecipe/pwa
